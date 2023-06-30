669184cee5cadabdb26a8a8b8ce0c838f2b9a1f3
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-06-30 19:50:03 +1200

    building forms

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,9 @@
 import Form from './Form'
-import Sentiment from 'sentiment'
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <h1>My Homie</h1>
       <p>React development has begun!</p>
       <Form />
     </div>
